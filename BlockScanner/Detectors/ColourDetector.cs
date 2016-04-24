f36e7af8ee5bec97787401fb1d05d72090c60890
Chris <chris.e.j.ellis@gmail.com>
2016-04-24 22:43:26 +0100

    Quick Colour Detector (Requires refactor/code sharing).

---
+++ BlockScanner/Detectors/ColourDetector.cs
@@ -0,0 +1,39 @@
+﻿namespace BlockScanner.Detectors
+{
+    using System.Drawing;
+
+    public class ColourDetector : BaseDetector<Color>
+    {
+        public override Color Detect(byte[] bitmapData, int x, int y)
+        {
+            var index = CoordinatesToIndex(x, y);
+
+            Color pixelColor = Color.FromArgb(
+                //pixelSize == 3 ? 255 : rgbValues[index + 3], // A component if present
+                255, // Assume our alpha is always 255 (we aren't capturing alpha in our bitmaps).
+                bitmapData[index + 2], // R component
+                bitmapData[index + 1], // G component
+                bitmapData[index]      // B component
+                );
+
+            // Simple stab at colour detection.
+            if (pixelColor.R > 110
+                || pixelColor.B > 110
+                || pixelColor.G > 100
+                || (pixelColor.R + pixelColor.G > 100) && pixelColor.B < 20 // Take a punt at the yellows/oranges.
+                )
+                return pixelColor;
+
+            return Color.Black;
+        }
+
+        public override void HighlightSamplePoints(byte[] rgbValues, int x, int y)
+        {
+            var index = CoordinatesToIndex(x, y);
+
+            rgbValues[index + 2] = 255;
+            rgbValues[index + 1] = 255;
+            rgbValues[index] = 255;
+        }
+    }
+}

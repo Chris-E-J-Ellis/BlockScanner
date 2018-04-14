be390458287d66f0c6c0e1d3d7ccdcf90d4712d8
Chris <chris.e.j.ellis@gmail.com>
2018-04-14 12:59:25 +0100

    Refactoring the scaleable selection region. Use thumb controls.

--- BlockScanner.Wpf/Views/PersistentCaptureWindow.xaml.cs
+++ BlockScanner.Wpf/Views/PersistentCaptureWindow.xaml.cs
@@ -15,20 +15,12 @@
     // Capture technique based on this article: http://www.abhisheksur.com/2010/04/screen-capture-using-wpf-winforms.html
     public partial class PersistentCaptureWindow : Window
     {
-        private double x;
-        private double y;
-        private double width;
-        private double height;
-        private bool isMouseDown = false;
-        private Point startPoint;
-
         public double SelectionX { get; private set; }
         public double SelectionY { get; private set; }
         public double SelectionWidth { get; private set; }
         public double SelectionHeight { get; private set; }
 
         public Rect SelectionArea { get; private set; }
-        public Rectangle CaptureRect { get; private set; }
         public CaptureRegionViewModel CaptureRegion { get; private set; } = new CaptureRegionViewModel();
 
         public System.Drawing.Rectangle SelectionAreaRectangle
@@ -41,145 +33,15 @@
             // Allow window to maximise on any monitor.
             // http://stackoverflow.com/questions/3121900/how-can-i-make-a-wpf-window-maximized-on-the-screen-with-the-mouse-cursor
 
-            //this.SourceInitialized += (s, a) => this.WindowState = WindowState.Maximized;
+            this.SourceInitialized += (s, a) => this.WindowState = WindowState.Maximized;
         }
 
-        private void Window_PreviewMouseMove(object sender, MouseEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            return;
-            if (this.isMouseDown)
-            {
-                double curx = e.GetPosition(null).X;
-                double cury = e.GetPosition(null).Y;
+            var screenCoordinates = this.PointToScreen(new Point(Canvas.GetLeft(CaptureTest.Region), Canvas.GetTop(CaptureTest.Region)));
+            this.SelectionArea = new Rect(screenCoordinates, new Size(CaptureTest.Region.Width, CaptureTest.Region.Height));
 
-                System.Windows.Shapes.Rectangle r = new System.Windows.Shapes.Rectangle();
-                SolidColorBrush brush = new SolidColorBrush(Colors.White);
-                r.Stroke = brush;
-                r.Fill = brush;
-                r.StrokeThickness = 1;
-
-                var recWidth = (curx - x);
-                var recHeight = (cury - y);
-
-                r.Width = Math.Abs(recWidth);
-                r.Height = Math.Abs(recHeight);
-
-                canvas.Children.Clear();
-                canvas.Children.Add(r);
-
-                if (recWidth <= 0)
-                    Canvas.SetLeft(r, curx);
-                else
-                    Canvas.SetLeft(r, x);
-
-                if (recHeight <= 0)
-                    Canvas.SetTop(r, cury);
-                else
-                    Canvas.SetTop(r, y);
-
-                if (e.LeftButton == MouseButtonState.Released)
-                {
-                    canvas.Children.Clear();
-                    width = e.GetPosition(null).X - x;
-                    height = e.GetPosition(null).Y - y;
-                    this.x = this.y = 0;
-                    this.isMouseDown = false;
-
-                    // Convert from Wpf coordinates;
-                    Point topLeft = this.PointToScreen(new Point(Canvas.GetLeft(r), Canvas.GetTop(r)));
-
-                    this.SelectionHeight = r.Height;
-                    this.SelectionWidth = r.Width;
-                    this.SelectionX = topLeft.X;
-                    this.SelectionY = topLeft.Y;
-
-                    this.SelectionArea = new Rect(topLeft, new Size(r.Width, r.Height));
-
-                    this.Close();
-                }
-            }
-        }
-
-        private void Thumb_DragDelta(object sender, DragDeltaEventArgs e)
-        {
-            //Move the Thumb to the mouse position during the drag operation
-            var myThumb = sender as Thumb;
-            var thumbX = Canvas.GetLeft(myThumb);
-            var thumbY = Canvas.GetTop(myThumb);
-
-            if (e.HorizontalChange != 0 || e.VerticalChange != 0)
-            {
-                var newThumbX = thumbX + e.HorizontalChange;
-                var newThumbY = thumbY + e.VerticalChange;
-                Canvas.SetLeft(myThumb, newThumbX);
-                Canvas.SetTop(myThumb, newThumbY);
-
-                var newRectX = Canvas.GetLeft(MyRectangle);
-                var newRectY = Canvas.GetTop(MyRectangle);
-                var newRectWidth = MyRectangle.Width;
-                var newRectHeight = MyRectangle.Height;
-
-                // Resize rectangle.
-                switch (myThumb.Name)
-                {
-                    case "myThumbTL":
-                        newRectX = newThumbX;
-                        newRectY = newThumbY;
-                        newRectWidth = newRectWidth - e.HorizontalChange;
-                        newRectHeight = newRectHeight - e.VerticalChange;
-                        break;
-                    case "myThumbTR":
-                        newRectY = newThumbY;
-                        newRectWidth = newRectWidth + e.HorizontalChange;
-                        newRectHeight = newRectHeight - e.VerticalChange;
-                        break;
-                    case "myThumbBL":
-                        newRectX = newThumbX;
-                        newRectWidth = newRectWidth - e.HorizontalChange;
-                        newRectHeight = newRectHeight + e.VerticalChange;
-                        break;
-                    case "myThumbBR":
-                        newRectWidth = newRectWidth + e.HorizontalChange;
-                        newRectHeight = newRectHeight + e.VerticalChange;
-                        break;
-                    case "MyRectangle":
-                        newRectX = newThumbX;
-                        newRectY = newThumbY;
-                        break;
-                    default:
-                        break;
-                }
-
-                // Set Size
-                MyRectangle.Width = newRectWidth >= 0 ? newRectWidth : MyRectangle.Width;
-                MyRectangle.Height = newRectHeight >= 0 ? newRectHeight : MyRectangle.Height;
-
-                // Set Set Position 
-                Canvas.SetLeft(MyRectangle, newRectX);
-                Canvas.SetTop(MyRectangle, newRectY);
-
-                // Reposition handles
-                UpdateHandlePositions();
-            }
-        }
-
-        public void UpdateHandlePositions()
-        {
-            var rectX = Canvas.GetLeft(MyRectangle);
-            var rectY = Canvas.GetTop(MyRectangle);
-            var rectWidth = MyRectangle.Width;
-            var rectHeight = MyRectangle.Height;
-            var translationX = 10;
-            var translationY = 10;
-
-            Canvas.SetLeft(MyThumbTL, rectX);
-            Canvas.SetLeft(MyThumbBL, rectX);
-            Canvas.SetLeft(MyThumbTR, rectX + rectWidth - translationX);
-            Canvas.SetLeft(MyThumbBR, rectX + rectWidth - translationX);
-            Canvas.SetTop(MyThumbTL, rectY);
-            Canvas.SetTop(MyThumbTR, rectY);
-            Canvas.SetTop(MyThumbBL, rectY + rectHeight - translationY);
-            Canvas.SetTop(MyThumbBR, rectY + rectHeight - translationY);
+            this.Close();
         }
     }
 }
---
+++ BlockScanner.Wpf/Views/ResizableRegion.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace BlockScanner.Wpf.Views
+{
+    /// <summary>
+    /// Interaction logic for ResizableRegion.xaml
+    /// </summary>
+    public partial class ResizableRegion : UserControl
+    {
+        public ResizableRegion()
+        {
+            InitializeComponent();
+        }
+
+        private void Thumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            //Move the Thumb to the mouse position during the drag operation
+            var thumb = sender as Thumb;
+
+            var newThumbX = Canvas.GetLeft(thumb) + e.HorizontalChange;
+            var newThumbY = Canvas.GetTop(thumb) + e.VerticalChange;
+
+            Canvas.SetLeft(thumb, newThumbX);
+            Canvas.SetTop(thumb, newThumbY);
+
+            var newRegionX = Canvas.GetLeft(Region);
+            var newRegionY = Canvas.GetTop(Region);
+            var newRegionWidth = Region.Width;
+            var newRegionHeight = Region.Height;
+
+            // Resize rectangle.
+            switch (thumb.Name)
+            {
+                case "ThumbTL":
+                    newRegionX = newThumbX;
+                    newRegionY = newThumbY;
+                    newRegionWidth = newRegionWidth - e.HorizontalChange;
+                    newRegionHeight = newRegionHeight - e.VerticalChange;
+                    break;
+                case "ThumbTR":
+                    newRegionY = newThumbY;
+                    newRegionWidth = newRegionWidth + e.HorizontalChange;
+                    newRegionHeight = newRegionHeight - e.VerticalChange;
+                    break;
+                case "ThumbBL":
+                    newRegionX = newThumbX;
+                    newRegionWidth = newRegionWidth - e.HorizontalChange;
+                    newRegionHeight = newRegionHeight + e.VerticalChange;
+                    break;
+                case "ThumbBR":
+                    newRegionWidth = newRegionWidth + e.HorizontalChange;
+                    newRegionHeight = newRegionHeight + e.VerticalChange;
+                    break;
+                case "Region":
+                    newRegionX = newThumbX;
+                    newRegionY = newThumbY;
+                    break;
+                default:
+                    break;
+            }
+
+
+            UpdateRegion(newRegionX, newRegionY, newRegionWidth, newRegionHeight);
+            UpdateHandlePositions();
+        }
+
+        private void UpdateRegion(double x, double y, double width, double height)
+        {
+            Canvas.SetLeft(Region, x);
+            Canvas.SetTop(Region, y);
+
+            Region.Width = width >= 0 ? width : Region.Width;
+            Region.Height = height >= 0 ? height : Region.Height;
+        }
+
+        private void UpdateHandlePositions()
+        {
+            var regionX = Canvas.GetLeft(Region);
+            var regionY = Canvas.GetTop(Region);
+            var regionWidth = Region.Width;
+            var regionHeight = Region.Height;
+            var translationX = 10;
+            var translationY = 10;
+
+            Canvas.SetLeft(ThumbTL, regionX);
+            Canvas.SetLeft(ThumbBL, regionX);
+            Canvas.SetLeft(ThumbTR, regionX + regionWidth - translationX);
+            Canvas.SetLeft(ThumbBR, regionX + regionWidth - translationX);
+            Canvas.SetTop(ThumbTL, regionY);
+            Canvas.SetTop(ThumbTR, regionY);
+            Canvas.SetTop(ThumbBL, regionY + regionHeight - translationY);
+            Canvas.SetTop(ThumbBR, regionY + regionHeight - translationY);
+        }
+    }
+}
